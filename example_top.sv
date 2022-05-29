51bc6f3713e9da36f06df56d90daac705452bc09
Xark <Xark@XarkLabs.com>
2022-05-29 16:12:05 -0700

    fix comments (thanks Duality), tweak ifdefs

--- example_top.sv
+++ example_top.sv
@@ -47,7 +47,7 @@ assign unused_ext_clk = &{ 1'b0, gpio_20, (CLOCK_HZ == 0 ? 1'b0 : 1'b0) };
 
 always_comb     clk = gpio_20;
 `else              // else !EXT_CLK
-`ifdef SYNTHESIS   // if synthesizing design for FPGA
+`ifndef VERILATOR   // don't use primitives with VERILATOR
 // Lattice documentation for iCE40UP5K oscillators:
 // https://www.latticesemi.com/-/media/LatticeSemi/Documents/ApplicationNotes/IK/iCE40OscillatorUsageGuide.ashx?document_id=50670
 /* verilator lint_off PINMISSING */ // suppress warnings about "missing pin" (default okay here)
@@ -64,25 +64,25 @@ SB_HFOSC  #(
 localparam NS_48M   =   (1_000_000_000 / CLOCK_HZ) / 2;   // delay for CLOCK_HZ frequency clk toggle
 // suppress verilog warnings that are okay during simulation
 /* verilator lint_off STMTDLY */
-/* verilator lint_off INFINITELOOP */
+
 initial begin
     clk = '0;               // set initial value (or it will be 'X', and !'X' is still 'X'...)
-    forever                 // forever loop
-        #(NS_48M)   clk = !clk; // delay ns, then toggle clock
+end
+
+always begin
+    #(NS_48M)   clk = !clk; // delay ns, then toggle clock
 end
 
 logic unused_ext_clk;
 assign unused_ext_clk = &{ 1'b0, gpio_20};
 
 /* verilator lint_on STMTDLY */
-/* verilator lint_on INFINITELOOP */
-`endif              // end !SYNTHESIS
+`endif              // end !VERILATOR
 `endif              // end !EXT_CLK
 
 // === input setup
 
 `ifndef VERILATOR
-/* verilator lint_off PINMISSING */
 SB_IO #(
     .PIN_TYPE(6'b0000_01),
     .PULLUP(1'b1)
@@ -90,7 +90,6 @@ SB_IO #(
     .PACKAGE_PIN({gpio_2, gpio_46, gpio_47 }),
     .D_IN_0({ button[0], button[1], button[2] })
 );
-/* verilator lint_on PINMISSING */
 `else
 // directly assign buttons in simulation
 always_comb begin
@@ -104,7 +103,7 @@ end
 // NOTE: This primitive not strictly needed, but by using SB_RGBA_DRV it allows
 // us to turn the UPduino RGB LED "photon cannons" power level down to stun (so
 // you don't need goggles). :)
-`ifdef SYNTHESIS    // if synthesizing design for FPGA
+`ifndef VERILATOR
 // RGB open-drain, high-current LED output
 SB_RGBA_DRV #(
     .RGB0_CURRENT("0b000001"),  // set current for LEDs (lowest @ (4mA/2)=2mA)
@@ -112,9 +111,9 @@ SB_RGBA_DRV #(
     .RGB2_CURRENT("0b000001")
 ) rgb_driver (
     .RGBLEDEN(1'b1),    // enable RGB LED output
-    .RGB0PWM(green),    // turn red LED on/off
-    .RGB1PWM(blue),     // turn green LED on/off
-    .RGB2PWM(red),      // turn blue LED on/off
+    .RGB0PWM(green),    // turn green LED on/off
+    .RGB1PWM(blue),     // turn blue LED on/off
+    .RGB2PWM(red),      // turn red LED on/off
     .CURREN(1'b1),      // 0=full current, 1=half current
     .RGB0(led_green),   // connection to green LED pin
     .RGB1(led_blue),    // connection to blue LED pin
